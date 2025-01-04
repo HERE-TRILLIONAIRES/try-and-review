95ccddf8631f480bbc6f7772e80fa1d654bc40f2
신영한 <syhan7516@naver.com>
2025-01-05 03:54:29 +0900

    feat: Review 엔티티 생성 #8

---
+++ com.trillionares.tryit.review/src/main/java/com/trillionares/tryit/review/domain/model/Review.java
@@ -0,0 +1,50 @@
+package com.trillionares.tryit.review.domain.model;
+
+import com.trillionares.tryit.review.domain.common.base.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@Table(name = "p_review")
+@Builder(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class Review extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "review_id")
+    private UUID reviewId;
+
+    @Column(name = "user_id", nullable = false)
+    private UUID userId;
+
+    @Column(name = "product_id", nullable = false)
+    private UUID productId;
+
+    @Column(name = "review_title", nullable = false)
+    private String reviewTitle;
+
+    @Column(name = "review_content", columnDefinition = "TEXT", nullable = false)
+    private String reviewContent;
+
+    @Column(name = "review_score", nullable = false)
+    private int reviewScore;
+
+    @Column(name = "review_img_url", nullable = false)
+    private String reviewImgUrl;
+
+    public static Review of(UUID userId, UUID productId, String reviewTitle, String reviewContent, int reviewScore, String reviewImgUrl) {
+        return Review.builder()
+                .userId(userId)
+                .productId(productId)
+                .reviewTitle(reviewTitle)
+                .reviewContent(reviewContent)
+                .reviewScore(reviewScore)
+                .reviewImgUrl(reviewImgUrl)
+                .build();
+    }
+}
