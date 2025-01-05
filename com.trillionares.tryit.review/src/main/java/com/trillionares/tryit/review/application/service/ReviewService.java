5fe5cc3a8ada637937724789e8b5a51e7f98cc08
신영한 <syhan7516@naver.com>
2025-01-06 01:20:14 +0900

    feat: 리뷰 단건 API 구현 #36

--- com.trillionares.tryit.review/src/main/java/com/trillionares/tryit/review/application/service/ReviewService.java
+++ com.trillionares.tryit.review/src/main/java/com/trillionares/tryit/review/application/service/ReviewService.java
@@ -2,12 +2,14 @@ package com.trillionares.tryit.review.application.service;
 
 import com.trillionares.tryit.review.application.dto.request.ReviewCreateRequestDto;
 import com.trillionares.tryit.review.application.dto.response.ReviewCreateResponseDto;
+import com.trillionares.tryit.review.application.dto.response.ReviewGetResponseDto;
 import com.trillionares.tryit.review.domain.model.Review;
 import com.trillionares.tryit.review.domain.repository.ReviewRepository;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -19,5 +21,11 @@ public class ReviewService {
     public ReviewCreateResponseDto createReview(ReviewCreateRequestDto reviewCreateRequestDto) {
         Review review = reviewRepository.save(Review.of(reviewCreateRequestDto.userId(),reviewCreateRequestDto.productId(),reviewCreateRequestDto.reviewTitle(),reviewCreateRequestDto.reviewContent(),reviewCreateRequestDto.reviewScore(),"image_url"));
         return ReviewCreateResponseDto.of(review.getReviewId(),review.getCreatedAt());
+    }
+
+    @Transactional(readOnly = true)
+    public ReviewGetResponseDto getReview(UUID reviewId) {
+        Review review = reviewRepository.findById(reviewId).get();
+        return ReviewGetResponseDto.from(review);
     }
 }
--- com.trillionares.tryit.review/src/main/java/com/trillionares/tryit/review/presentation/controller/ReviewController.java
+++ com.trillionares.tryit.review/src/main/java/com/trillionares/tryit/review/presentation/controller/ReviewController.java
@@ -2,14 +2,15 @@ package com.trillionares.tryit.review.presentation.controller;
 
 import com.trillionares.tryit.review.application.dto.request.ReviewCreateRequestDto;
 import com.trillionares.tryit.review.application.dto.response.ReviewCreateResponseDto;
+import com.trillionares.tryit.review.application.dto.response.ReviewGetResponseDto;
 import com.trillionares.tryit.review.application.service.ReviewService;
 import com.trillionares.tryit.review.presentation.dto.BaseResponse;
 import com.trillionares.tryit.review.presentation.dto.ReviewCreateRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,5 +21,10 @@ public class ReviewController {
     @PostMapping("/api/reviews")
     public BaseResponse<ReviewCreateResponseDto> createReview(@RequestBody ReviewCreateRequest reviewCreateRequest) {
         return BaseResponse.of(201,HttpStatus.CREATED,"리뷰 생성 성공",reviewService.createReview(ReviewCreateRequestDto.from(reviewCreateRequest)));
+    }
+
+    @GetMapping("/api/reviews/{reviewId}")
+    public BaseResponse<ReviewGetResponseDto> getReview(@PathVariable UUID reviewId) {
+        return BaseResponse.of(200,HttpStatus.OK,"리뷰 단건 조회 성공",reviewService.getReview(reviewId));
     }
 }
