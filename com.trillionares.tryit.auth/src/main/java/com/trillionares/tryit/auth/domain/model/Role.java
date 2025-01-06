4b32c672f7dcf8d4e9dd640545bff6ad1c49fd1e
hiimsajo <hiimsajo@gmail.com>
2025-01-06 21:24:07 +0900

    ✨update: auth서비스는 임시로 비워둠, AuthApplication.java는 @EnableJpaAuditing이 이중 빈으로 등록돼서 오류나길래 삭제함, Role에 권한관련 추가

--- com.trillionares.tryit.auth/src/main/java/com/trillionares/tryit/auth/domain/model/Role.java
+++ com.trillionares.tryit.auth/src/main/java/com/trillionares/tryit/auth/domain/model/Role.java
@@ -1,7 +1,20 @@
 package com.trillionares.tryit.auth.domain.model;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum Role {
-  ADMIN,
-  COMPANY,
-  MEMBER
+  ADMIN(Authority.ADMIN),
+  COMPANY(Authority.COMPANY),
+  MEMBER(Authority.MEMBER);
+
+  private final String authority;
+
+  public static class Authority {
+    public static final String ADMIN = "ADMIN";
+    public static final String COMPANY = "COMPANY";
+    public static final String MEMBER = "MEMBER";
+  }
 }
