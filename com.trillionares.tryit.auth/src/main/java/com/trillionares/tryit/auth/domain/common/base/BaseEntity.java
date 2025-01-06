beb04314689eb10372ae38f63c8df0dbb951c3ba
hiimsajo <hiimsajo@gmail.com>
2025-01-06 21:08:23 +0900

    ✨add: 감사필드를 위한 Base Entity 관련 파일들 추가

---
+++ com.trillionares.tryit.auth/src/main/java/com/trillionares/tryit/auth/domain/common/base/BaseEntity.java
@@ -0,0 +1,46 @@
+package com.trillionares.tryit.auth.domain.common.base;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@Setter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+
+  @CreatedDate
+  @Column(name = "created_at", updatable = false)
+  private LocalDateTime createdAt;
+
+  @CreatedBy
+  @Column(name = "created_by", updatable = false)
+  private String createdBy;
+
+  @LastModifiedDate
+  @Column(name = "updated_at")
+  private LocalDateTime updatedAt;
+
+  @LastModifiedBy
+  @Column(name = "updated_by")
+  private String updatedBy;
+
+  @Column(name = "deleted_at")
+  private LocalDateTime deletedAt;
+
+  @Column(name = "deleted_by")
+  private String deletedBy;
+
+  @Column(name = "is_delete", nullable = false)
+  private boolean isDelete;
+
+}
