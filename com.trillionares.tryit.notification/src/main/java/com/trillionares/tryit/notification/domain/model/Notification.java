306d5bb19444f889c1c710cbdd8f865f01b86aa8
yjson <handyejee14@gmail.com>
2025-01-07 01:34:25 +0900

    feat : 알림 생성 로직 구현 - 체험단에서 이벤트 consumer 생성해 받아오는 로직 구현 - 알림 생성 로직 구현

---
+++ com.trillionares.tryit.notification/src/main/java/com/trillionares/tryit/notification/domain/model/Notification.java
@@ -0,0 +1,60 @@
+package com.trillionares.tryit.notification.domain.model;
+
+import com.trillionares.tryit.notification.domain.common.base.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.util.UUID;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Table(name = "p_notification")
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Notification extends BaseEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  @Column(name = "notification_id", updatable = false)
+  private UUID notificationId;
+
+  @Column(name = "user_id", columnDefinition = "uuid")
+  private UUID userId;
+
+  @Column(name = "submission_id", columnDefinition = "uuid")
+  private UUID submissionId;
+
+  @Column(name = "notification_status")
+  private NotificationStatus notificationStatus;
+
+  @Column(name = "attempt_count")
+  private Integer attemptCount = 0;
+
+  public void increaseAttemptCount() {
+    this.attemptCount++;
+    updateStatusBasedOnAttempt();
+  }
+
+  public void updateStatusBasedOnAttempt() {
+    if (this.attemptCount >= 3) { // 0, 1, 2 최대 3회
+      this.notificationStatus = NotificationStatus.FAILED;
+    }
+  }
+
+  @Builder
+  public Notification(UUID notificationId, UUID userId, UUID submissionId,
+      NotificationStatus notificationStatus, Integer attemptCount
+  ) {
+    this.notificationId = notificationId;
+    this.userId = userId;
+    this.submissionId = submissionId;
+    this.notificationStatus = NotificationStatus.PENDING; // 초기상태 PENDING
+    this.attemptCount = attemptCount;
+  }
+}
