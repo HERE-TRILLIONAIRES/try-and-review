e20e05b6bcbf26702b33dbbd8709210b3de1f274
Hany-Kim <hanykim1210@gmail.com>
2025-01-08 08:15:22 +0900

    :sparkles: feat: trial create API

--- com.trillionares.tryit.trial/src/main/java/com/trillionares/tryit/trial/jhtest/presentation/controller/trialController.java
+++ com.trillionares.tryit.trial/src/main/java/com/trillionares/tryit/trial/jhtest/presentation/controller/trialController.java
@@ -5,6 +5,7 @@ import com.trillionares.tryit.trial.jhtest.presentation.dto.TrialIdResponseDto;
 import com.trillionares.tryit.trial.jhtest.presentation.dto.TrialInfoRequestDto;
 import com.trillionares.tryit.trial.jhtest.presentation.dto.common.base.BaseResponseDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,7 +23,7 @@ public class trialController {
 
         TrialIdResponseDto responseDto = trialService.createTrial(requestDto);
 
-        return null;
+        return BaseResponseDto.from(HttpStatus.CREATED.value(), HttpStatus.CREATED, "신청을 완료했습니다.", responseDto);
     }
 
 }
