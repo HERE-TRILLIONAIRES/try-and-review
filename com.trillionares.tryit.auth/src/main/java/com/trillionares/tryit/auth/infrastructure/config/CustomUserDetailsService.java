d0b795397ef1c0f2416eca107094f9c890b8b309
hiimsajo <hiimsajo@gmail.com>
2025-01-07 20:42:17 +0900

    ✨update: id에서 userId로 변경 및 findByUsernameAndIsDeletedFalse로 수정

--- com.trillionares.tryit.auth/src/main/java/com/trillionares/tryit/auth/infrastructure/config/CustomUserDetailsService.java
+++ com.trillionares.tryit.auth/src/main/java/com/trillionares/tryit/auth/infrastructure/config/CustomUserDetailsService.java
@@ -17,7 +17,7 @@ public class CustomUserDetailsService implements UserDetailsService {
 
   @Override
   public CustomUserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-    User user = userRepository.findByUsername(username)
+    User user = userRepository.findByUsernameAndIsDeletedFalse(username)
         .orElseThrow(() -> new UsernameNotFoundException("Not Found username: " + username));
 
     return new CustomUserDetails(user);
