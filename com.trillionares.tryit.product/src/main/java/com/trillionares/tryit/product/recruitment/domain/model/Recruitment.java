ee0aacf85747d17d84fe1efb3f00ece437c9ffa0
Namgyu11 <103015031+Namgyu11@users.noreply.github.com>
2025-01-08 14:25:39 +0900

    fix: Recruitment 엔티티의 테이블 스키마 정보 추가

--- com.trillionares.tryit.product/src/main/java/com/trillionares/tryit/product/recruitment/domain/model/Recruitment.java
+++ com.trillionares.tryit.product/src/main/java/com/trillionares/tryit/product/recruitment/domain/model/Recruitment.java
@@ -8,7 +8,7 @@ import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity
-@Table(name = "p_recruitment")
+@Table(name = "p_recruitment", schema = "product")
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
