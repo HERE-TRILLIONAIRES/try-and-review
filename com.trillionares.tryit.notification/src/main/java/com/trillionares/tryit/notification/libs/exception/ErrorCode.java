2c8ef9e345aa4c5f8caec9a0f54c670d51a84188
yjson <handyejee14@gmail.com>
2025-01-09 23:16:11 +0900

    refactor: 알림 예외 추가

--- com.trillionares.tryit.notification/src/main/java/com/trillionares/tryit/notification/libs/exception/ErrorCode.java
+++ com.trillionares.tryit.notification/src/main/java/com/trillionares/tryit/notification/libs/exception/ErrorCode.java
@@ -24,7 +24,8 @@ public enum ErrorCode {
 
   // notification
   INVALID_SUBMISSION_STATUS(400, "허용되지 않는 체험단 신청 상태입니다."),
-  NOTIFICATION_NOT_FOUND(404, "해당 알림은 존재하지 않습니다.");
+  NOTIFICATION_NOT_FOUND(404, "해당 알림은 존재하지 않습니다."),
+  SLACK_NOTIFICATION_FAILED(500, "알림 생성이 실패되었습니다.");
 
   private final int status;
   private final String message;
