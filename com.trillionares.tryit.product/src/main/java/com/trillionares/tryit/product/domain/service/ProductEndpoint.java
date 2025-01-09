22e5a4c4f5813cc24bc8d11f99a4d32569489321
Hany-Kim <hanykim1210@gmail.com>
2025-01-09 20:23:48 +0900

    :sparkles: feat: Create Product API - using kafka when image data saved to DB

---
+++ com.trillionares.tryit.product/src/main/java/com/trillionares/tryit/product/domain/service/ProductEndpoint.java
@@ -0,0 +1,38 @@
+package com.trillionares.tryit.product.domain.service;
+
+import com.trillionares.tryit.product.domain.common.json.JsonUtils;
+import com.trillionares.tryit.product.domain.model.product.Product;
+import com.trillionares.tryit.product.domain.repository.ProductRepository;
+import com.trillionares.tryit.product.presentation.dto.ProductIdAndProductImageIdDto;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class ProductEndpoint {
+
+    private final ProductRepository productRepository;
+
+    @Transactional
+    @KafkaListener(groupId = "productImageManagement", topics = "updateImageIdOfProduct")
+    public void updateImageIdOfProduct(String message) throws Exception {
+        log.info("updateImageIdOfProduct: {}", message);
+
+        ProductIdAndProductImageIdDto pidAndPimgidDto = JsonUtils.fromJson(message, ProductIdAndProductImageIdDto.class);
+
+        Optional<Product> product = productRepository.findByProductIdAndIsDeleteFalse(pidAndPimgidDto.getProductId());
+
+        if (!product.isPresent() || product == null) {
+            throw new Exception("Product Not Found");
+        }
+
+        product.get().setProductImgId(pidAndPimgidDto.getProductImageId());
+
+        productRepository.save(product.get());
+    }
+}
