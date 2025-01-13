96075a78f8532ec8ac2b2702813650cf076d49f7
yjson <handyejee14@gmail.com>
2025-01-14 00:43:27 +0900

    fix : 사용자 동기 통신 부분 헤더 부분 추가할 수 있도록 수정

--- com.trillionares.tryit.notification/src/main/java/com/trillionares/tryit/notification/libs/client/auth/AuthServiceClient.java
+++ com.trillionares.tryit.notification/src/main/java/com/trillionares/tryit/notification/libs/client/auth/AuthServiceClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-@FeignClient(name = "auth-service")
+@FeignClient(name = "auth-service" , url = "${openapi.service.url}")
 public interface AuthServiceClient {
 
   @GetMapping("/users/{userId}")
