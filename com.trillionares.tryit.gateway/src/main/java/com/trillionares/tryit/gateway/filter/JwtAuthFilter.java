3f8b6632351a3e075d6df6cec08af221d38ce412
hiimsajo <hiimsajo@gmail.com>
2025-01-14 21:30:27 +0900

    ✨refactoring: jwt 검증 후에 헤더에 유저네임과 롤을 넣도록 수정

--- com.trillionares.tryit.gateway/src/main/java/com/trillionares/tryit/gateway/filter/JwtAuthFilter.java
+++ com.trillionares.tryit.gateway/src/main/java/com/trillionares/tryit/gateway/filter/JwtAuthFilter.java
@@ -30,36 +30,17 @@ public class JwtAuthFilter implements GatewayFilter {
       return chain.filter(exchange);
     }
 
-    // JWT 추출
     String token = jwtUtil.extractToken(exchange.getRequest());
-    if (token == null) {
-      log.warn("JWT 없음");
-      exchange.getResponse().setStatusCode(HttpStatus.BAD_REQUEST); // 잘못된 요청
-      return exchange.getResponse().setComplete();
-    }
-
-    try {
-      // JWT 검증 및 사용자 정보 추출
-      String userName = jwtUtil.extractUsername(token);
-      String userRole = jwtUtil.extractRole(token);
-      log.info("사용자 이름: {}, 역할: {}", userName, userRole);
-
-      // 사용자 정보를 헤더에 추가하고 필터 체인 계속
-      return chain.filter(addHeaders(exchange, userName, userRole));
-
-    } catch (io.jsonwebtoken.ExpiredJwtException e) {
-      log.error("JWT 만료: {}", e.getMessage());
-      exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED); // 인증 실패
-      return exchange.getResponse().setComplete();
-    } catch (io.jsonwebtoken.SignatureException e) {
-      log.error("JWT 서명 불일치: {}", e.getMessage());
-      exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
-      return exchange.getResponse().setComplete();
-    } catch (Exception e) {
-      log.error("JWT 검증 실패: {}", e.getMessage());
+    if (token == null || !jwtUtil.validateToken(token)) {
+      log.error("유효하지 않은 JWT");
       exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
       return exchange.getResponse().setComplete();
     }
+
+    // JWT 유효성 검증 후 사용자 정보를 헤더에 추가
+    String userName = jwtUtil.extractUsername(token);
+    String userRole = jwtUtil.extractRole(token);
+    return chain.filter(addHeaders(exchange, userName, userRole));
   }
 
   // 사용자 정보 헤더에 추가
