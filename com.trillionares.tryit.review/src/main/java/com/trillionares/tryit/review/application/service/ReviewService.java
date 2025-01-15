e4ac59ff985fa8c4db60b0c7632d5d57e1532680
신영한 <syhan7516@naver.com>
2025-01-15 11:12:20 +0900

    feat: 체험단 신청 당첨 여부 및 결과에 따른 제한 코드 추가 #99

--- com.trillionares.tryit.review/src/main/java/com/trillionares/tryit/review/application/service/ReviewService.java
+++ com.trillionares.tryit.review/src/main/java/com/trillionares/tryit/review/application/service/ReviewService.java
@@ -2,20 +2,20 @@ package com.trillionares.tryit.review.application.service;
 
 import com.trillionares.tryit.review.application.dto.request.ReviewCreateRequestDto;
 import com.trillionares.tryit.review.application.dto.request.ReviewUpdateRequestDto;
-import com.trillionares.tryit.review.application.dto.response.ReviewCreateResponseDto;
-import com.trillionares.tryit.review.application.dto.response.ReviewDeleteResponseDto;
-import com.trillionares.tryit.review.application.dto.response.ReviewGetResponseDto;
-import com.trillionares.tryit.review.application.dto.response.ReviewUpdateResponseDto;
+import com.trillionares.tryit.review.application.dto.response.*;
 import com.trillionares.tryit.review.domain.client.TrialClient;
 import com.trillionares.tryit.review.domain.model.Review;
 import com.trillionares.tryit.review.domain.repository.ReviewRepository;
 import com.trillionares.tryit.review.libs.exception.ErrorCode;
 import com.trillionares.tryit.review.libs.exception.GlobalException;
+import com.trillionares.tryit.review.presentation.dto.BaseResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.UUID;
+
+import static com.trillionares.tryit.review.libs.exception.ErrorCode.REVIEW_CREATE_FORBIDDEN;
 
 @Service
 @RequiredArgsConstructor
@@ -26,7 +26,14 @@ public class ReviewService {
 
     @Transactional
     public ReviewCreateResponseDto createReview(ReviewCreateRequestDto reviewCreateRequestDto) {
+        BaseResponse<ReviewIsSelectedResponseDto> reviewIsSelectedResponseDto
+                = trialClient.isSelectedStatusOfTrial(reviewCreateRequestDto.submissionId());
+
+        if(!reviewIsSelectedResponseDto.getData().getIsSelected())
+            throw new GlobalException(REVIEW_CREATE_FORBIDDEN);
+
         Review review = reviewRepository.save(Review.of(reviewCreateRequestDto.userId(),reviewCreateRequestDto.productId(),reviewCreateRequestDto.reviewTitle(),reviewCreateRequestDto.reviewContent(),reviewCreateRequestDto.reviewScore(),"image_url"));
+
         return ReviewCreateResponseDto.of(review.getReviewId(),review.getCreatedAt());
     }
 
