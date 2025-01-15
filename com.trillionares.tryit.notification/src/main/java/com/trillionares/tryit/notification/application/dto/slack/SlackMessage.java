82cbafc85afccd9ad185f8120875d26b1b2e2909
yjson <handyejee14@gmail.com>
2025-01-15 11:50:06 +0900

    refactor: 슬랙 메세지 상태에 따라 동적으로 송신하도록 개선

--- com.trillionares.tryit.notification/src/main/java/com/trillionares/tryit/notification/application/dto/slack/SlackMessage.java
+++ com.trillionares.tryit.notification/src/main/java/com/trillionares/tryit/notification/application/dto/slack/SlackMessage.java
@@ -1,21 +1,36 @@
 package com.trillionares.tryit.notification.application.dto.slack;
 
 import com.trillionares.tryit.notification.domain.model.Notification;
+import com.trillionares.tryit.notification.libs.exception.ErrorCode;
+import com.trillionares.tryit.notification.libs.exception.GlobalException;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
 public class SlackMessage {
+  private String channel;
   private String text;
-  private String submissionId;
-  private String userId;
 
-  public static SlackMessage from(Notification notification) {
+  public static SlackMessage from(Notification notification, String slackId, String status) {
+    String message = createMessageByStatus(status);
+
     return SlackMessage.builder()
-        .text("체험단으로 선정되셨습니다!")
-        .submissionId(notification.getSubmissionId().toString())
-        .userId(notification.getUserId().toString())
+        .channel(slackId)
+        .text(String.format("<@%s> %s\n제출 ID: %s",
+            slackId,
+            message,
+            notification.getSubmissionId()))
         .build();
+  }
+
+  private static String createMessageByStatus(String status) {
+    return switch (status) {
+      case "APPLIED" -> "체험단 신청이 완료되었습니다.";
+      case "SELECTED" -> "체험단에 선정되었습니다";
+      case "CANCELLED" -> "체험단 신청이 취소되었습니다";
+      case "FAILED" -> "체험단에 선정되지 않았습니다";
+      default -> throw new GlobalException(ErrorCode.INVALID_SUBMISSION_STATUS);
+    };
   }
 }
