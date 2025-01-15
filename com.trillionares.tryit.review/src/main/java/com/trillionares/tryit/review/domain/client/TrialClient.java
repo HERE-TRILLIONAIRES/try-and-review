3b48b36c152ab894bedfa1e0f100be2f743aee3f
신영한 <syhan7516@naver.com>
2025-01-15 11:11:24 +0900

    feat: 체험단 신청 당첨 여부에 대한 OpenFeign 설정 및 응답 Dto 생성 #99

--- com.trillionares.tryit.review/src/main/java/com/trillionares/tryit/review/domain/client/TrialClient.java
+++ com.trillionares.tryit.review/src/main/java/com/trillionares/tryit/review/domain/client/TrialClient.java
@@ -1,8 +1,16 @@
 package com.trillionares.tryit.review.domain.client;
 
+import com.trillionares.tryit.review.application.dto.response.ReviewIsSelectedResponseDto;
+import com.trillionares.tryit.review.presentation.dto.BaseResponse;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.UUID;
 
 @FeignClient(name="recruitment-service")
 public interface TrialClient {
 
+    @GetMapping("/api/submissions/{submissionId}/isSelected")
+    BaseResponse<ReviewIsSelectedResponseDto> isSelectedStatusOfTrial(@PathVariable("submissionId") UUID submissionId);
 }
