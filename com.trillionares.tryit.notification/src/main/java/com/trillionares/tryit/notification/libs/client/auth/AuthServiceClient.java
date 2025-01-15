9a48a038b100cee687cd967e7caa831a1778bdc3
yjson <handyejee14@gmail.com>
2025-01-15 11:51:26 +0900

    fix : public 부분 인터페이스에서 제거

--- com.trillionares.tryit.notification/src/main/java/com/trillionares/tryit/notification/libs/client/auth/AuthServiceClient.java
+++ com.trillionares.tryit.notification/src/main/java/com/trillionares/tryit/notification/libs/client/auth/AuthServiceClient.java
@@ -5,11 +5,10 @@ import java.util.UUID;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestHeader;
 
 @FeignClient(name = "auth-service" , url = "${openapi.service.url}")
 public interface AuthServiceClient {
 
   @GetMapping("users/internals/{userId}")
-  public BaseResponse<UserResponseDto> getUserInfo(@PathVariable UUID userId);
+   BaseResponse<UserResponseDto> getUserInfo(@PathVariable UUID userId);
 }
