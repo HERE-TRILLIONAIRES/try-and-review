8800708e2b8880d28f756fe9151279aa1a735759
yjson <handyejee14@gmail.com>
2025-01-16 15:55:20 +0900

    fix : InternalFeignClientConfig에 우선순위 가지도록 설정 추가

--- com.trillionares.tryit.notification/src/main/java/com/trillionares/tryit/notification/libs/client/auth/AuthServiceClient.java
+++ com.trillionares.tryit.notification/src/main/java/com/trillionares/tryit/notification/libs/client/auth/AuthServiceClient.java
@@ -1,12 +1,17 @@
 package com.trillionares.tryit.notification.libs.client.auth;
 
+import com.trillionares.tryit.notification.libs.client.config.InternalFeignClientConfig;
 import com.trillionares.tryit.notification.presentation.dto.BaseResponse;
 import java.util.UUID;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "auth-service", url = "${openapi.service.url}")
+@FeignClient(
+    name = "auth-service",
+    url = "${openapi.service.url}",
+    configuration = InternalFeignClientConfig.class
+)
 public interface AuthServiceClient {
 
   @GetMapping("users/internals/{userId}")
