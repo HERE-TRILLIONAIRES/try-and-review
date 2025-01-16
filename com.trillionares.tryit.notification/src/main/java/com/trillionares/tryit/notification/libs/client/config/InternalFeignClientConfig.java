572f212f0f1371ea41d46792220c70ce2454d40c
yjson <handyejee14@gmail.com>
2025-01-16 15:54:03 +0900

    fix : RequestInterceptor 권한 체크하는 부분 전체 feign client에 적용되고 있었던 부분 수정 - configuration 어노테이션 제거 (전체 적용되고 있었던 부분)

--- com.trillionares.tryit.notification/src/main/java/com/trillionares/tryit/notification/libs/client/config/InternalFeignClientConfig.java
+++ com.trillionares.tryit.notification/src/main/java/com/trillionares/tryit/notification/libs/client/config/InternalFeignClientConfig.java
@@ -7,12 +7,14 @@ import org.springframework.context.annotation.Configuration;
 /**
  * 내부 통신용
  */
-@Configuration
+
 public class InternalFeignClientConfig {
 
   @Bean
   public RequestInterceptor internalRequestInterceptor() {
-    return requestTemplate ->
-        requestTemplate.header("X-Internal-Call", "true");
+    return requestTemplate -> {
+      requestTemplate.headers().clear(); // 기존 헤더 제거
+      requestTemplate.header("X-Internal-Call", "true");
+    };
   }
 }
