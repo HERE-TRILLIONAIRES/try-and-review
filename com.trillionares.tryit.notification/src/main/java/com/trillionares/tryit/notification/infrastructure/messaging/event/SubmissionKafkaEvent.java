61c813302a19e4ee3a2d07679c1d04dd1f5c2d90
yjson <handyejee14@gmail.com>
2025-01-16 15:50:26 +0900

    fix : consumer 에서 역직렬화 두번 해오도록 수정

--- com.trillionares.tryit.notification/src/main/java/com/trillionares/tryit/notification/infrastructure/messaging/event/SubmissionKafkaEvent.java
+++ com.trillionares.tryit.notification/src/main/java/com/trillionares/tryit/notification/infrastructure/messaging/event/SubmissionKafkaEvent.java
@@ -5,8 +5,8 @@ import java.util.UUID;
 import lombok.Getter;
 import lombok.ToString;
 
-@Getter
 @ToString
+@Getter
 public class SubmissionKafkaEvent {
 
   private UUID submissionId;
@@ -14,26 +14,5 @@ public class SubmissionKafkaEvent {
   private UUID recruitmentId;
   private String status;
   private LocalDateTime submissionTime;
-  private UUID messageId;
-  private LocalDateTime eventTimestamp;
 
-  protected SubmissionKafkaEvent() {}
-
-  public SubmissionKafkaEvent(
-      UUID submissionId,
-      UUID userId,
-      UUID recruitmentId,
-      String status,
-      LocalDateTime submissionTime,
-      UUID messageId,
-      LocalDateTime eventTimestamp
-      ) {
-    this.submissionId = submissionId;
-    this.userId = userId;
-    this.recruitmentId = recruitmentId;
-    this.status = status;
-    this.submissionTime = submissionTime;
-    this.messageId = messageId;
-    this.eventTimestamp = eventTimestamp;
-  }
 }
