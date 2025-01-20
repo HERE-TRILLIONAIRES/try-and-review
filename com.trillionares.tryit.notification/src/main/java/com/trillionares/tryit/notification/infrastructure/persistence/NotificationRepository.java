904cc2c818fa3fc82012b6b0e6fa45166832a30b
yjson <handyejee14@gmail.com>
2025-01-20 18:36:49 +0900

    refactor: optional 로 messageId 확인하도록 변경

--- com.trillionares.tryit.notification/src/main/java/com/trillionares/tryit/notification/infrastructure/persistence/NotificationRepository.java
+++ com.trillionares.tryit.notification/src/main/java/com/trillionares/tryit/notification/infrastructure/persistence/NotificationRepository.java
@@ -2,6 +2,7 @@ package com.trillionares.tryit.notification.infrastructure.persistence;
 
 import com.trillionares.tryit.notification.domain.model.Notification;
 import com.trillionares.tryit.notification.domain.repository.NotificationRepositoryCustom;
+import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -12,6 +13,6 @@ public interface NotificationRepository extends
     JpaRepository<Notification, UUID>,
     NotificationRepositoryCustom {
 
-  boolean existsByMessageId(String messageId);
+  Optional<Notification> findByMessageId(String messageId);
 
 }
