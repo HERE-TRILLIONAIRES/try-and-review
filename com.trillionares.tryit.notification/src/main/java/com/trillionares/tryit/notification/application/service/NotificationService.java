5d9b715b0a573ccd554e71fc1fdcfe5e7cb376f9
yjson <handyejee14@gmail.com>
2025-01-20 20:31:58 +0900

    refactor: 알림 생성 로직 수정 - createNotificationFromSubmissionEvent 메서드에서 알림 생성, 저장 auth 조회 모든 부분 담당하고 있어 메서드 분리

--- com.trillionares.tryit.notification/src/main/java/com/trillionares/tryit/notification/application/service/NotificationService.java
+++ com.trillionares.tryit.notification/src/main/java/com/trillionares/tryit/notification/application/service/NotificationService.java
@@ -32,23 +32,69 @@ public class NotificationService {
   private final AuthClient authClient;
   private final NotificationRoleValidation roleValidation;
 
+  /**
+   * Kafka 이벤트를 기반으로 알림을 생성하고 Slack으로 발송합니다.
+   * @param event 체험단 신청(submission) 카프카 이벤트를 통해 받아온 값
+   */
   @Transactional
   public void createNotificationFromSubmissionEvent(SubmissionKafkaEvent event) {
 
-    if (notificationRepository.existsByMessageId(event.getMessageId())) {
-      throw new GlobalException(ErrorCode.DUPLICATE_MESSAGE_ID);
-    }
+    validateNotification(event);
+    Notification notification = notificationRepository.save(
+        convertEventToNotification(event));
 
-    // 알림 엔티티 저장
-    Notification notification = convertEventToNotification(event);
-    Notification savedNotification = notificationRepository.save(notification);
-
-    String slackId = getSlackId(savedNotification);
-
-    // 슬랙 알림 발송
-    slackNotificationSender.sendNotification(savedNotification, slackId, event.getStatus());
+    sendNotificationToSlack(notification, event.getStatus());
   }
 
+  /**
+   * 알림 유효성을 검사합니다 (동일한 messageId로 발송된 알림이 있는지 확인, 실패 경우 재시도 횟수 검증)
+   * @param event 체험단 신청(submission) 카프카 이벤트를 통해 받아온 값
+   */
+  private void validateNotification(SubmissionKafkaEvent event) {
+    notificationRepository.findByMessageId(event.getMessageId())
+        .ifPresent(notification -> {
+          if (notification.getNotificationStatus() == NotificationStatus.SENT) { //
+            throw new GlobalException(ErrorCode.DUPLICATE_MESSAGE_ID);
+          }
+          validateRetryCount(notification);
+        });
+  }
+
+  /**
+   * 알림 재시도 횟수가 3회를 초과 했는지 확인합니다.
+   * @param notification 알림 객체
+   */
+  private void validateRetryCount(Notification notification) {
+    if (notification.getAttemptCount() >= 3) {
+      log.warn("재시도 횟수 3회 초과 {}", notification.getMessageId());
+      throw new GlobalException(ErrorCode.MAX_RETRY_EXCEEDED);
+    }
+  }
+
+  /**
+   * 생성된 알림을 Slack으로 발송합니다.
+   * @param notification 알림 객체
+   * @param status 알림 상태 정보
+   */
+  private void sendNotificationToSlack(Notification notification, String status) {
+    try {
+      String slackId = getSlackId(notification);
+      slackNotificationSender.sendNotification(notification, slackId, status);
+      notification.markAsDelivered();
+
+    } catch (Exception e) {
+      notification.increaseAttemptCount();
+      log.error("알림 발송 실패, attempt {}: {}",
+          notification.getAttemptCount(), e.getMessage());
+      throw e;
+    }
+  }
+
+  /**
+   * Slack ID를 구하는 메서드
+   * @param notification 알림 객체
+   * @return Auth Service에서 조회한 Slack ID
+   */
   private String getSlackId(Notification notification) {
 
     BaseResponse<FeignUserIdResponseDto> response = authClient.getUserId(
@@ -56,16 +102,19 @@ public class NotificationService {
 
     FeignUserIdResponseDto userDto = response.getData();
     String slackId = userDto.getSlackId();
-
     log.info("slackId {}", slackId);
 
     if (slackId == null || slackId.isEmpty()) { // TODO: Feign error 추가 필요
       throw new GlobalException(ErrorCode.SLACK_ID_NOT_FOUND);
     }
-
     return slackId;
   }
 
+  /**
+   * Kafka 이벤트 데이터를 알림 엔티티로 변환하는 메서드 입니다.
+   * @param event Kafka 이벤트 데이터
+   * @return 생성된 알림 데이터
+   */
   private Notification convertEventToNotification(SubmissionKafkaEvent event) {
 
     return Notification.builder()
