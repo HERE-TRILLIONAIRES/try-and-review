a24a2c3e97cdd42286e0cbe5cc5f515282934a3c
신영한 <syhan7516@naver.com>
2025-01-24 03:13:15 +0900

    feat: Recruitment FeignClient 및 Dto 생성 #240

--- com.trillionares.tryit.statistics/src/main/java/com/trillionares/tryit/statistics/domain/client/ProductClient.java
+++ com.trillionares.tryit.statistics/src/main/java/com/trillionares/tryit/statistics/domain/client/ProductClient.java
@@ -1,8 +1,10 @@
 package com.trillionares.tryit.statistics.domain.client;
 
+import com.trillionares.tryit.statistics.application.dto.response.StatisticsDataToRecruitmentResponseDto;
 import com.trillionares.tryit.statistics.application.dto.response.StatisticsGetProductInfoResponseDto;
 import com.trillionares.tryit.statistics.presentation.dto.BaseResponse;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -13,4 +15,7 @@ public interface ProductClient {
 
     @GetMapping("/api/products/{productId}")
     BaseResponse<StatisticsGetProductInfoResponseDto> getProductInfoStatisticsToProduct(@PathVariable("userId") UUID productId);
+
+    @GetMapping("/recruitments/product/{productId}")
+    ResponseEntity<StatisticsDataToRecruitmentResponseDto> getStatisticsDataToRecruitment(@PathVariable("productId") UUID productId);
 }
