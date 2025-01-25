16ce4a5f1a0c80e5c223e3514b2c2817bdf7a52e
신영한 <syhan7516@naver.com>
2025-01-26 04:44:01 +0900

    feat: Schedule Batch Job Launcher 등록 #278

--- com.trillionares.tryit.statistics/src/main/java/com/trillionares/tryit/statistics/libs/config/StatisticsScheduler.java
+++ com.trillionares.tryit.statistics/src/main/java/com/trillionares/tryit/statistics/libs/config/StatisticsScheduler.java
@@ -1,43 +1,32 @@
 package com.trillionares.tryit.statistics.libs.config;
-import com.trillionares.tryit.statistics.application.dto.response.StatisticsCreateDataResponseDto;
-import com.trillionares.tryit.statistics.domain.client.ProductClient;
-import com.trillionares.tryit.statistics.domain.client.ReviewClient;
-import com.trillionares.tryit.statistics.domain.model.Statistics;
-import com.trillionares.tryit.statistics.domain.respository.StatisticsRepository;
-import com.trillionares.tryit.statistics.presentation.dto.BaseResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
+import java.time.LocalDateTime;
 
 @Component
 @RequiredArgsConstructor
 public class StatisticsScheduler {
 
-    private final ReviewClient reviewClient;
-    private final ProductClient productClient;
-    private final StatisticsRepository statisticsRepository;
+    private final JobLauncher jobLauncher;
+    private final Job statisticsCreateJob;
 
     @Scheduled(cron = "00 00 00 * * *")
     @Transactional
-    public void createStatisticsSchedule() {
-
-        BaseResponse<List<StatisticsCreateDataResponseDto>>
-                statisticsCreateDataResponseDtoBaseResponse = reviewClient.getStatisticsDataToReview();
-
-        statisticsCreateDataResponseDtoBaseResponse.getData()
-                .forEach(statisticsDataDto
-                        -> statisticsRepository.save(Statistics.of(
-                        productClient.getProductInfoStatisticsToProduct(
-                                statisticsDataDto.getProductId()).getData().getUserId(),
-                        statisticsDataDto.getProductId(),
-                        statisticsDataDto.getHighestScore(),
-                        statisticsDataDto.getLowestScore(),
-                        statisticsDataDto.getAverageScore(),
-                        statisticsDataDto.getReviewCount(),
-                        productClient.getStatisticsDataToRecruitment(
-                                statisticsDataDto.getProductId()).getBody().getCompletionTime())));
+    public void createStatisticsSchedule() throws JobInstanceAlreadyCompleteException, JobExecutionAlreadyRunningException, JobParametersInvalidException, JobRestartException {
+        JobParameters jobParameters = new JobParametersBuilder()
+                .addString("date", LocalDateTime.now().toString())
+                .toJobParameters();
+        jobLauncher.run(statisticsCreateJob,jobParameters);
     }
 }
